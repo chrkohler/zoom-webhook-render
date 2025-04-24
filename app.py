c92d4a4d66b90243d83910593e589cacd57e6da6
chrkohler <43324930+chrkohler@users.noreply.github.com>
2025-04-24 11:57:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+'''from flask import Flask, request, jsonify
 import requests
 import os
 import logging
@@ -169,4 +169,13 @@ def webhook():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     app.logger.info(f"Iniciando servidor en puerto {port}")
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)'''
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    return '', 200
